85106b7fe0f26249ce7ac664cbd28f9fa723c917
robertokoba7 <okobarobert7@gmail.com>
2023-04-27 11:37:36 +0300

    [add] filtered logger file

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+from typing import List
+import re
+import logging
+from os import environ
+import mysql.connector
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
