6c97d0c95d01c320caaa4d548b8fb5a73a77fd64
robertokoba7 <okobarobert7@gmail.com>
2023-05-04 12:37:50 +0300

    [Feature]: error handler 401

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,12 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(401)
+@app.errorhandler(403)
 def not_found(error) -> str:
     """
-    Unauthorized handler
+    Forbidden handler
     """
-    return jsonify({"error": "Unauthorized"}), 401
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
